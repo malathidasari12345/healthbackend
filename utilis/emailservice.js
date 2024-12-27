526ab4a0d27307becbdc6f398381425bd1c41f19
malathi dasari <malathidasari30@gmail.com>
2024-12-27 14:50:38 +0530

    changes

--- utilis/emailservice.js
+++ utilis/emailservice.js
@@ -3,21 +3,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-const sendEmail = async (to, subject, text) => {
+const sendEmail = async (to, subject, content, isHtml = false) => {
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to,
     subject,
-    text,
+    [isHtml ? "html" : "text"]: content,
   };
 
   try {
